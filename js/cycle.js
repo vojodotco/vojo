337172985e6042c90dac7e2842c947d42e1d9e3c
squiggy rubio <squiggy@resist.ca>
2012-05-07 11:24:47 -0700

    fixes to photo and media mover panel pans

--- js/cycle.js
+++ js/cycle.js
@@ -1,7 +1,8 @@
 Drupal.behaviors.nodeCycle = function() {
   $('div.pane-blog-photos div.field-field-image div.field-items').cycle({
-    fx: 'fade',
-    timeout: 5000,
+    fx: 'scrollLeft',
+    speed:   700,
+    timeout: 6000,
     prev: '#vojo-node-cycle-prev',
     next: '#vojo-node-cycle-next, div.pane-blog-photos div.field-items',
     pager:  '#vojo-node-cycle-pager'
