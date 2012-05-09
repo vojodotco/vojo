ae6d14675ebaa9823ba5abd0d06e792e68223062
squiggy rubio <squiggy@resist.ca>
2012-05-09 16:25:47 -0700

    change pager on photos

--- js/cycle.js
+++ js/cycle.js
@@ -5,6 +5,13 @@ Drupal.behaviors.nodeCycle = function() {
     timeout: 6000,
     prev: '#vojo-node-cycle-prev',
     next: '#vojo-node-cycle-next, div.pane-blog-photos div.field-items',
-    pager:  '#vojo-node-cycle-pager'
+    pager:  '#vojo-node-cycle-pager',
+    after: onAfter
   });
+    
+    // To get current slide number for see this example: http://www.malsup.com/jquery/cycle/count.html
+  function onAfter(curr,next,opts) {
+    var blog_pager = (opts.currSlide + 1) + ' of ' + opts.slideCount;
+    $('#vojo-node-cycle-pager').html(blog_pager);
+  }
 }
