6768a9e870bfd165f0c7547a4f7f6e4c3175efc4
squiggy rubio <squiggy@resist.ca>
2012-06-04 14:44:18 -0700

    add template for top-header

---
+++ top-header.tpl.php
@@ -0,0 +1,13 @@
+<a id="beta" href="https://dev.vozmob.net/projects/cronicas/roadmap">beta</a>
+<div id="vojo-top-header">
+  <div class="container">
+    <div class="row">
+      <div class="span8">
+        <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
+      </div> <!-- /.span8 -->
+      <div class="span4">
+        <?php print $user_links; ?>
+      </div> <!-- /.span4 -->
+    </div> <!-- /.row -->
+  </div> <!-- /.container -->
+</div> <!-- /#vojo-top-header -->
