90ffdc2951e9d1c21a68e063e46693b852962704
Rahul Bhargava <rahulb@media.mit.edu>
2012-06-06 10:19:51 -0400

    site header style tweaks, renamed all to vojo-site-header (for clarity)

---
+++ templates/vojo-site-header.tpl.php
@@ -0,0 +1,7 @@
+<a id="beta" href="https://dev.vozmob.net/projects/cronicas/roadmap">beta</a>
+<div id="vojo-site-header">
+  <div class="container">
+    <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
+    <?php print $user_links; ?>
+  </div>
+</div> <!-- /#vojo-top-header -->
