6ba4cfa1ae8fbc47a2290f5cc3b6256e0bc0df04
Rahul Bhargava <rahulb@media.mit.edu>
2012-06-06 10:08:31 -0400

    added in row and spans, for bootstrap-css "correctness"

--- top-header.tpl.php
+++ top-header.tpl.php
@@ -1,7 +1,13 @@
 <a id="beta" href="https://dev.vozmob.net/projects/cronicas/roadmap">beta</a>
 <div id="vojo-top-header">
   <div class="container">
+    <div class="row">
+        <div class="span8">
         <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
+        </div>
+        <div class="span4">
         <?php print $user_links; ?>
-  </div> <!-- /.container -->
+        </div>
+    </div>
+  </div>
 </div> <!-- /#vojo-top-header -->
