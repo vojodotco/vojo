da3b96be253c0c501665f406b2edac6213f401b5
squiggy rubio <squiggy@resist.ca>
2012-06-13 17:57:09 -0700

    add custom panel panes for front and trynow pages, and jquery for front slideshow

--- js/cycle.js
+++ js/cycle.js
@@ -5,7 +5,6 @@ Drupal.behaviors.nodeCycle = function() {
     timeout: 6000,
     prev: '#vojo-node-cycle-prev',
     next: '#vojo-node-cycle-next, div.pane-blog-photos div.field-items',
-    pager:  '#vojo-node-cycle-pager',
     after: onAfter
   });
     
@@ -14,4 +13,21 @@ Drupal.behaviors.nodeCycle = function() {
     var blog_pager = (opts.currSlide + 1) + ' of ' + opts.slideCount;
     $('#vojo-node-cycle-pager').html(blog_pager);
   }
+
+  /*  $('#front-slideshow ul').cycle({
+    fx: 'fade',
+    speed:   700,
+    timeout: 0,
+    prev: '#front-slideshow #vojo-slide-prev',
+    next: '#front-slideshow #vojo-slide-next',
+    slideResize: 0
+  }); */
+    
+  $('#front-slideshow ul li.views-row-first').addClass("current-slide");
+  $('#front-slideshow #vojo-slide-next').click(function () {
+    $('#front-slideshow ul li.current-slide').not("#front-slideshow ul li.views-row-last").next().addClass("current-slide").prev().removeClass("current-slide");
+  });
+  $('#front-slideshow #vojo-slide-prev').click(function () {
+    $('#front-slideshow ul li.current-slide').not("#front-slideshow ul li.views-row-first").prev().addClass("current-slide").next().removeClass("current-slide");
+  });
 }
