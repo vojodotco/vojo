962e1d8d0182a3624895b0c42b7d282557ef76fc
squiggy rubio <squiggy@resist.ca>
2012-07-10 19:30:25 -0700

    add language switcher... we worked on the same thing at the same time. :p

--- templates/vojo-site-header.tpl.php
+++ templates/vojo-site-header.tpl.php
@@ -2,7 +2,7 @@
 <div id="vojo-site-header">
   <div class="container">
     <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
+    <?php print $language_switcher; ?>
     <?php print $user_links; ?>
-    <?php print $language_links; ?>
   </div>
 </div> <!-- /#vojo-top-header -->
