b922640d683db6cbf626133ef8f64a9c2134aa44
Edward L Platt <ed@elplatt.com>
2012-10-09 14:02:30 -0400

    Changes beta logo in vojo header to link to homepage, Re #1434.

--- templates/vojo-site-header.tpl.php
+++ templates/vojo-site-header.tpl.php
@@ -1,4 +1,4 @@
-<a id="beta" href="https://dev.vozmob.net/projects/cronicas/roadmap">beta</a>
+<a id="beta" href="<?php print $base_path; ?>">beta</a>
 <div id="vojo-site-header">
   <div class="container">
     <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
