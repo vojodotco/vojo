3c8f9f27e2dfcf521e4946743eb3952915a52032
Edward L Platt <ed@elplatt.com>
2012-12-11 12:37:38 -0500

    Adds phone number to site header #936.

--- templates/vojo-site-header.tpl.php
+++ templates/vojo-site-header.tpl.php
@@ -2,6 +2,7 @@
 <div id="vojo-site-header">
   <div class="container">
     <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
+    <div class="block-voip-number"><?php print $vojo_callin_number; ?></div>
     <?php print $language_switcher; ?>
     <?php print $user_links; ?>
   </div>
