7229a57968d2009df00fc1c8cbf9cc4b2f6dcff1
squiggy rubio <squiggy@mirabot.coop>
2013-06-10 15:56:05 -0700

    a few changes for new front page design

--- templates/vojo-site-header.tpl.php
+++ templates/vojo-site-header.tpl.php
@@ -3,7 +3,9 @@
   <div class="container">
     <a  id="vojo-logo" href="<?php print $base_path; ?>"><img src="<?php print $base_path . drupal_get_path('theme', 'vojo_generic'); ?>/images/vojo/logo-top-header.png" alt="Vojo home page" /></a>
     <div class="block-voip-number"><?php print $vojo_callin_number; ?></div>
-    <?php print $language_switcher; ?>
-    <?php print $user_links; ?>
+  <?php print $user_links; ?>
+  <?php if (isset($primary_links)) : ?>
+    <?php print theme('links', $primary_links, array('class' => 'links primary-links')) ?>
+  <?php endif; ?>
   </div>
 </div> <!-- /#vojo-top-header -->
